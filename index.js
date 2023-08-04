b234b546104a2519f770bd6ae2a294fe32ba1e9c
sea <sea@fish.local>
2023-08-03 20:50:47 -0700

    New updated services

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function detectService(apiKey) {
     const Facebook_Access_Token = /^EAACEdEose0cBA[0-9A-Za-z]+$/;
     const Facebook_OAuth_20 = /^[A-Za-z0-9]{125}$/;
     const Instagram_OAuth_20 = /^[0-9a-fA-F]{7}.[0-9a-fA-F]{32}$/;
-    const regexGoogleMaps = /^AIza[0-9A-Za-z-_]{35}$/;
-    const regexYoutube = /^AIza[0-9A-Za-z-_]{35}$/;
+    const Google_Api_Key = /^AIza[0-9A-Za-z-_]{35}$/;
     const regexGithub = /^ghp_[a-zA-Z0-9]{36}$/;
 
     const services = {
@@ -27,8 +26,7 @@ function detectService(apiKey) {
         "Facebook Access Token":Facebook_Access_Token,
         "Facebook OAuth 2.0":Facebook_OAuth_20,
         "Instagram OAuth 2.0":Instagram_OAuth_20,
-        "Google Maps":regexGoogleMaps,
-        "Youtube":regexYoutube,
+        "Google API Key":Google_Api_Key ,
         "Github": regexGithub,
         // Puedes agregar más servicios y sus regex aquí
     };
